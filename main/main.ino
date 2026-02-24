64506c7f67b838d5cc692465e8597f687f808c85
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-24 11:53:12 +0700

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,18 @@
+#include <wifi.h>
+
+// Tipe data di arduino
+
+int ledPin; // bilangan bulat 2 byte -32.768 sampai 32.768
+char text = "Hello World!"; // char bisa digunakan untuk string
+bool nilai = 0, 1, false, true; // itu adalah binnary atau binner
+long nilai1 = -2.146.483.648; // long bilangan bulat 4 byte -2.146.483.648 sampai -2.146.483.647 
+float nilai2 = -3,4028235; // float bilangan real -3,4028235 x 10^38 sampai 3,-3,4028235 x 10^38
+double nilai3 = 1.123.123; // double bilangan real yang buanyaaakkk
+
+
 
 void setup() {
-  serial
+  printl(Text);
 
 }
 
